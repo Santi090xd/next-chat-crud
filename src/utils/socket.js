483b65db0ad25a4eb812b7ce418f1db8b5a5cdc3
Santi090 <santi090pro@gmail.com>
2024-03-18 14:20:19 -0300

    socket path fixed?
    just removed the slash

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
 
-const socket = io("https://next-chat-server-eight.vercel.app/")
+const socket = io("https://next-chat-server-eight.vercel.app")
 
 export default socket
