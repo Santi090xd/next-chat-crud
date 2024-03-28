5ea98a6f382fe4c825cb848d55bd57023dc0f02f
Santi090 <santi090pro@gmail.com>
2024-03-28 18:43:26 -0300

    Changing server address
    :)

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
 
-const socket = io("https://next-chat-server-eight.vercel.app")
+const socket = io("https://ac5034f6-3d45-4788-bae4-0d122012a788-00-2458xxnkiz34f.janeway.replit.dev")
 
 export default socket
